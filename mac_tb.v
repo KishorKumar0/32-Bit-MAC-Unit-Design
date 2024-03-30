b5815c39fb54157dc38f851853467bac901618db
Kishor Kumar <144250655+KishorKumar0@users.noreply.github.com>
2024-03-31 02:20:27 +0530

    Update mac_tb.v

--- mac_tb.v
+++ mac_tb.v
@@ -26,33 +26,34 @@ module mac_tb;
     
     reg [31:0] multiplicand;
     reg [31:0] multiplier;
-    reg [63:0] accumulator_in;
     wire [64:0] accumulator_out;
 
     reg clk = 0;
-    
-    mac dut (
+    reg reset;
+    mac Accunaltor (
         .multiplicand(multiplicand),
         .multiplier(multiplier),
-        .accumulator_in(accumulator_in),
+        .clk(clk),
+        .reset(reset),
         .accumulator_out(accumulator_out)
     );
 
     always #((CLK_PERIOD / 2)) clk = ~clk;
 
     initial begin
-        multiplicand = 32'h12345678;
-        multiplier = 32'h87654321;
-        accumulator_in = 64'h0001;
-
-        #100 multiplicand = 32'h11111111; multiplier = 32'h11111111; accumulator_in = 64'h11111111;
+        reset = 1;
+        multiplicand = 32'h11111111;
+        multiplier = 32'h11111111;
+        
+       #10 reset = 0;
+        #100 multiplicand = 32'h12345678; multiplier = 32'h87654321;
 
         #100 $finish;
     end
 
     always @(posedge clk) begin
-        $display("Time = %0t: multiplicand = %h, multiplier = %h, accumulator_in = %h, accumulator_out = %d",
-                 $time, multiplicand, multiplier, accumulator_in, accumulator_out);
+        $display("Time = %0t: multiplicand = %d, multiplier = %d, accumulator_out = %d",
+                 $time, multiplicand, multiplier, accumulator_out);
     end
 
 endmodule
