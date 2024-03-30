bd7525dd0dee123b92684698bff47680423b9990
Kishor Kumar <144250655+KishorKumar0@users.noreply.github.com>
2024-03-31 02:19:59 +0530

    Update mac.v

--- mac.v
+++ mac.v
@@ -4,8 +4,8 @@
 // Engineer: Kishor Kumar
 // 
 // Create Date: 30.03.2024 01:31:58
-// Design Name: MAC Unit
-// Module Name: MAC Module
+// Design Name: MAC unit
+// Module Name: mac
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -23,22 +23,27 @@
 module mac(
     input [31:0] multiplicand,
     input [31:0] multiplier,
-    input [63:0] accumulator_in,
+    input clk, reset,
     output reg [64:0] accumulator_out
 );
-
     wire [64:0] multiplication_result;
     wire [65:0] addition_result;
+    reg [63:0] accumulator;
 
     // Perform the multiplication using a 16x16 Vedic multiplier
-    vedic_32x32 mult(multiplicand, multiplier, multiplication_result);
+ 
+    vedic_32x32 multi(multiplicand, multiplier, multiplication_result);
 
     // Perform the addition of the multiplication result and the accumulator input
-    adder adder(multiplication_result, accumulator_in, addition_result);
+    adder adder(multiplication_result, accumulator_out, addition_result);
 
     // Output the final result as the accumulator output
-    always @* begin
-        accumulator_out = addition_result;
+    always @(posedge clk) begin
+        if(reset) 
+            accumulator_out <= 64'b0;  
+        else begin
+            accumulator_out <= addition_result[64:0];
+        end
     end
 
 endmodule
